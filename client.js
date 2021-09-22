a0d0b107307cd669f813fcfb3d4946e0a2cbba9a
wei-paul <paul.wei607@gmail.com>
2021-09-22 20:45:28 +0000

    experimented with setInterval

--- client.js
+++ client.js
@@ -4,9 +4,15 @@ const connect = function () {
     host: "135.23.223.133",
     port: 50542
   });
+  conn.setEncoding("utf8");
 
   // interpret incoming data as text
-  conn.setEncoding("utf8");
+  conn.on("connect", (connect) => {
+    conn.write("Name: BLA")
+    console.log("Successfully connected to game server")
+
+  })
+  
   conn.on("data", (data) => {
     console.log(data)
   });
