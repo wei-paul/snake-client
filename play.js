3ddd338b2405d2b8c0182f197c5ec1c4cbf00172
wei-paul <paul.wei607@gmail.com>
2021-09-22 21:36:42 +0000

    added ctrl c

--- play.js
+++ play.js
@@ -1,5 +1,25 @@
+const stdin = process.stdin;
 const connect = require('./client')
-// establishes a connection with the game server
-connect();
+
 
 console.log("Connecting ...");
+connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+  const handleUserInput = function (data) {
+    if (data === '\u0003') {
+
+      process.exit();
+
+  }  
+};
+  
+  setupInput();
+  stdin.on("data", handleUserInput);
+
